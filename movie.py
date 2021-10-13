722b56dc6ad1c933c5423ae07c135f7c589516b3
pontakornth <pontakorn61@gmail.com>
2021-10-13 21:46:07 +0700

    refactor: Delegate price to PriceCode

--- movie.py
+++ movie.py
@@ -1,23 +1,51 @@
-class Movie:
-	"""
-	A movie available for rent.
-	"""
-	# The types of movies (price_code). 
-	REGULAR = 0
-	NEW_RELEASE = 1
-	CHILDRENS = 2
-	
-	def __init__(self, title, price_code):
-		# Initialize a new movie. 
-		self.title = title
-		self.price_code = price_code
+from enum import Enum
 
-	def get_price_code(self):
-		# get the price code
-		return self.price_code
-	
-	def get_title(self):
-		return self.title
-	
-	def __str__(self):
-		return self.title
+
+class PriceCode(Enum):
+    regular = {
+        "base": 2.0,
+        "base_days": 2,
+        "day_price": 1.5
+    }
+
+    children = {
+        "base": 1.5,
+        "base_days": 3,
+        "day_price": 1.5
+    }
+
+    new_release = {
+        "base": 0,
+        "base_days": 0,
+        "day_price": 3
+    }
+
+    def price(self, days: int):
+        """Get price base on days rented."""
+        price_code = self.value
+        if days < price_code["base_days"]:
+            return price_code["base"]
+        price = price_code["base"] + price_code["day_price"] * (days - price_code["base_days"])
+        return price
+
+
+class Movie:
+    """
+    A movie available for rent.
+    """
+    # The types of movies (price_code).
+
+    def __init__(self, title, price_code: PriceCode):
+        # Initialize a new movie.
+        self.title = title
+        self.price_code = price_code
+
+    def get_price_code(self):
+        # get the price code
+        return self.price_code
+
+    def get_title(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
--- rental.py
+++ rental.py
@@ -1,6 +1,6 @@
 import logging
 
-from movie import Movie
+from movie import PriceCode
 
 
 class Rental:
@@ -29,27 +29,15 @@ class Rental:
         return self.days_rented
 
     def get_price(self):
-        amount = 0
-        if self.get_movie().get_price_code() == Movie.REGULAR:
-            # Two days for $2, additional days 1.50 each.
-            amount = 2.0
-            if self.get_days_rented() > 2:
-                amount += 1.5 * (self.get_days_rented() - 2)
-        elif self.get_movie().get_price_code() == Movie.CHILDRENS:
-            # Three days for $1.50, additional days 1.50 each.
-            amount = 1.5
-            if self.get_days_rented() > 3:
-                amount += 1.5 * (self.get_days_rented() - 3)
-        elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-            # Straight per day charge
-            amount = 3 * self.get_days_rented()
-        else:
-            log = logging.getLogger()
-            log.error(
-                f"Movie {self.get_movie()} has unrecognized priceCode {self.get_movie().get_price_code()}")
-        return amount
+        price_code = self.get_movie().get_price_code()
+        return price_code.price(self.get_days_rented())
+        # else:
+        #     log = logging.getLogger()
+        #     log.error(
+        #         f"Movie {self.get_movie()} has unrecognized priceCode {self.get_movie().get_price_code()}")
+        # return amount
 
     def get_rental_points(self):
-        if self.get_movie().get_price_code() == Movie.NEW_RELEASE:
+        if self.get_movie().get_price_code() == PriceCode.new_release:
             return self.get_days_rented()
         return 1
