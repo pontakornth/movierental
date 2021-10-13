b3f20ffefa1bc4232cc7c8f2633dac1d080ae5ff
pontakornth <pontakorn61@gmail.com>
2021-10-13 20:52:21 +0700

    refactor: Move rental calculation to method

--- customer.py
+++ customer.py
@@ -39,24 +39,7 @@ class Customer:
 
         for rental in self.rentals:
             # compute rental change
-            amount = 0
-            if rental.get_movie().get_price_code() == Movie.REGULAR:
-                # Two days for $2, additional days 1.50 each.
-                amount = 2.0
-                if rental.get_days_rented() > 2:
-                    amount += 1.5 * (rental.get_days_rented() - 2)
-            elif rental.get_movie().get_price_code() == Movie.CHILDRENS:
-                # Three days for $1.50, additional days 1.50 each.
-                amount = 1.5
-                if rental.get_days_rented() > 3:
-                    amount += 1.5 * (rental.get_days_rented() - 3)
-            elif rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
-                # Straight per day charge
-                amount = 3 * rental.get_days_rented()
-            else:
-                log = logging.getLogger()
-                log.error(
-                    f"Movie {rental.get_movie()} has unrecognized priceCode {rental.get_movie().get_price_code()}")
+            amount = rental.calculate_charge()
             # award renter points
             if rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
                 frequent_renter_points += rental.get_days_rented()
