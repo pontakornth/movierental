7c629e29e035a8ec634172a1a4e9ad955f807960
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2021-11-05 21:53:48 +0700

    feat: Introduce movie catalog

--- movie.py
+++ movie.py
@@ -1,16 +1,5 @@
-from typing import List
-
-
-def create_linear_price_formula(base_days, base_price, price_per_day):
-    """Create a calculation function based on base days and price."""
-
-    def linear_formula(days_rented):
-        """Linear formula of price"""
-        if days_rented < base_days:
-            return base_price
-        return base_price + price_per_day * (days_rented - base_days)
-
-    return linear_formula
+from typing import List, Dict
+import csv
 
 
 class Movie:
@@ -37,3 +26,48 @@ class Movie:
 
     def __str__(self):
         return self.__title
+
+
+class MovieCatalog:
+    """
+    Catalog for creating movies from CSV file.
+    """
+
+    def __init__(self, filename="movies.csv"):
+        """Create instance of MovieCatalog and load csv. file.
+
+        Args:
+            filename (str): Name of CSV file containing data of movies """
+        self.__file = open(filename)
+        # Dictionary of movies, keyed by name.
+        self.__movies = dict()
+
+    def __scan(self, title: str) -> Dict:
+        """Find movies by name.
+
+        It will try to search in dictionary first.
+        If there is no such movie, it reads from file.
+        Args:
+            title (str): Movie name to search for
+        Returns:
+            Dict: Movie data from CSV
+        Raises:
+            KeyError: When there is no such movie.
+        """
+        movie_data = self.__movies.get(title)
+        if movie_data is None:
+            for row in csv.DictReader(self.__file):
+                row_title = row["title"]
+                self.__movies[row_title] = row
+                if title == row_title:
+                    return row
+            raise KeyError("The movie does not exist in the database.")
+        return movie_data
+
+    def get_movie(self, movie_title: str) -> Movie:
+        """Read CSV and return movies from it."""
+        movie_data = self.__scan(movie_title)
+        genre_list = movie_data["genres"].split("|")
+        year = int(movie_data["year"])
+        movie = Movie(movie_data["title"], year, genre_list)
+        return movie
